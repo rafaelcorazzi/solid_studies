897f6e5b86f0bb650197a7e1678db57847a28190
Rafael Corazzi <rafael.corazzi@hotmail.com>
2021-10-25 21:33:43 -0300

    most code sucks

--- FileStorage.cs
+++ FileStorage.cs
@@ -1,7 +1,18 @@
 public class FileStorage
 {
     public string WorkingDirectory {get;set;}
-    public string Save(int id, string message);
+    public string Save(int id, string message)
+    {
+        var path = Path.Combine(this.WorkingDirectory, $"{id}.txt");
+        File.WriteAllText(path, message);
+        return path;
+
+    }
     public event EventHandler<MessageEventArgs> MessageRead;
-    public void Read(int id);
+    public void Read(int id)
+    {
+        var path = Path.Combine(this.WorkingDirectory, $"{id}.txt");
+        var msg = File.ReadAllText(path);
+        this.MessageRead(this, new MessageEventArgs {Message = msg});
+    }
 }
