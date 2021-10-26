f7ed0a6e3a0a7292d4a61b284dc8e2aecbc14350
Rafael Corazzi <rafael.corazzi@hotmail.com>
2021-10-26 08:46:37 -0300

    using maybe for better null treatment

--- FileStorage.cs
+++ FileStorage.cs
@@ -1,6 +1,13 @@
 public class FileStorage
 {
-    public string WorkingDirectory {get;set;}
+    public FileStorage(string workingDirectory)
+    {
+        if (workingDirectory == null)
+            throw new ArgumentNullException("is null"); 
+            
+        this.WorkingDirectory = workingDirectory;
+    }
+    public string WorkingDirectory {get;private set;}
     public string Save(int id, string message)
     {
         var path = this.GetFileName(id);
@@ -10,9 +17,13 @@ public class FileStorage
     }
     public void Read(int id)
     {
+
         var path = this.GetFileName(id);
+        if (!File.Exists(path))
+            return new Maybe<string>();
+
         var msg = File.ReadAllText(path);
-        return msg;
+        return new Maybe<string>(msg);
     }
 
     public string GetFileName(int id){
